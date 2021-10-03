11ae83b53b77ecb363752a0cae1b34f1e020ea88
Oleg Cheremisin <cheremisin@gmail.com>
2021-10-04 04:12:58 +0500

    redicers fix

--- HW1/3/reducer_mean.py
+++ HW1/3/reducer_mean.py
@@ -2,10 +2,11 @@ import sys
 
 for line in sys.stdin:
         cj, mj = list(map(float, line.split()))
+        break
 
 for line in sys.stdin:
         ck, mk = list(map(float, line.split()))
-        mi = (cj*mj + ck*mk) / (cj + ck)
+        mi = (cj * mj + ck * mk) / (cj + ck)
         cj, mj = ck, mi
 
 print(mj)
--- HW1/3/reducer_var.py
+++ HW1/3/reducer_var.py
@@ -2,11 +2,12 @@ import sys
 
 for line in sys.stdin:
         cj, mj, vj = list(map(float, line.split()))
+        break
 
 for line in sys.stdin:
         ck, mk, vk = list(map(float, line.split()))
         cjk = cj + ck
-        vi = ((cj*vj + ck*vk) / cjk) + cj * ck * ((mj - mk)/cjk)**2
+        vi = ((cj * vj + ck * vk) / cjk) + cj * ck * ((mj - mk) / cjk) ** 2
         cj, mj, vj = ck, mk, vi
 
 print(vj)
