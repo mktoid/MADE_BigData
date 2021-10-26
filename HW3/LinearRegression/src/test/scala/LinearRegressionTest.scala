ba30cab9ba33f696a725327db8e13c974d25ee9a
Oleg Cheremisin <cheremisin@gmail.com>
2021-10-26 22:49:22 +0500

    cleanup

--- HW3/LinearRegression/src/test/scala/LinearRegressionTest.scala
+++ HW3/LinearRegression/src/test/scala/LinearRegressionTest.scala
@@ -8,9 +8,9 @@ import matchers._
 
 class LinearRegressionTest extends AnyFlatSpec with should.Matchers {
 
-  "LinearRegression predict length" should "match number of rows in test dataset" in {
-    var xTrain = DenseVector[Double](1.66666667, -3.88888889, -1.66666667, -0.55555556,  3.88888889)
-    var yTrain = DenseVector[Double](13.78720563, -4.27348569,  5.34625839,  2.27038372, 17.65965573)
+  "Predict length" should "match number of rows in test dataset" in {
+    val xTrain = DenseVector[Double](1.66666667, -3.88888889, -1.66666667, -0.55555556,  3.88888889)
+    val yTrain = DenseVector[Double](13.78720563, -4.27348569,  5.34625839,  2.27038372, 17.65965573)
     var reg = new LinearRegression
     reg.fit(xTrain.toDenseMatrix.t, yTrain)
     val pred = reg.predict(xTrain.toDenseMatrix.t)
